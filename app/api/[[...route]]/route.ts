bb2ea8149bc3a8bee0e7f753cc15504f0109b21d
Mirsoip <mirsoip.adilov@gmail.com>
2024-07-09 16:37:00 +0500

    Honojs setup section finished. Video stopped at 1.10.46

---
+++ app/api/[[...route]]/route.ts
@@ -0,0 +1,17 @@
+import { z } from 'zod'
+import { Hono } from 'hono'
+import { handle } from 'hono/vercel'
+import { zValidator } from '@hono/zod-validator'
+
+import authors from "./authors"
+import books from "./books"
+
+export const runtime = 'edge'
+
+const app = new Hono().basePath('/api')
+
+app.route("/authors", authors)
+app.route("/books", books)
+
+export const GET = handle(app)
+export const POST = handle(app)
