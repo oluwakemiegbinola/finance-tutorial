d7c2c90416a754be8e4c2c201c72f0f3fc64ef43
Mirsoip <mirsoip.adilov@gmail.com>
2024-07-12 17:00:12 +0500

    Header component part is finished. Video stopped at 1.46.44

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -3,15 +3,12 @@ import { Hono } from 'hono'
 import { handle } from 'hono/vercel'
 import { zValidator } from '@hono/zod-validator'
 
-import authors from "./authors"
-import books from "./books"
-
 export const runtime = 'edge'
 
 const app = new Hono().basePath('/api')
 
-app.route("/authors", authors)
-app.route("/books", books)
-
+app.get("/hello", (c) => {
+  return c.json({ hello: "World" })
+})
 export const GET = handle(app)
 export const POST = handle(app)
